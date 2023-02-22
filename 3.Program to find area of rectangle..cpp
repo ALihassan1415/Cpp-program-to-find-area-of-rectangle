8613506277e44122895c64562e0b090f57aa7de9
Ali Hassan <125749746+ALihassan1415@users.noreply.github.com>
2023-02-22 09:35:50 +0500

    Add files via upload

---
+++ 3.Program to find area of rectangle..cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main()
+{
+	int l,w;
+	float area;
+	cout<<"Enter the length of Rectangle : ";
+	cin>>l;
+	cout<<"Enter the Width of Rectangle : ";
+	cin>>w;
+	area=l*w;
+	cout<<"The area of a Rectangle is : "<<endl<<area<<" square inches";
+	return 0;
+}
